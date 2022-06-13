2337642239e724bb76a8ede6746f5fb4a23c306f
Illia Pospielov <ilia.pospielov@gmail.com>
2022-06-13 23:28:43 +0300

    google auth

--- src/hooks/googleAuth.ts
+++ src/hooks/googleAuth.ts
@@ -7,21 +7,24 @@ export const useGoogleAuth = () => {
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    // @ts-ignore
-    google.accounts.id.initialize({
-      client_id: `${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`,
-      callback: (response: any) => dispatch(googleAuth(response.credential)),
-    })
+    //@ts-ignore
+    if (google){
+      // @ts-ignore
+      google.accounts.id.initialize({
+        client_id: `${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`,
+        callback: (response: any) => dispatch(googleAuth(response.credential)),
+      })
 
-    // @ts-ignore
-    google.accounts.id.renderButton(
-      document.getElementById('google-login'),
-      {
-        theme: 'outline',
-        size: 'large',
-        locale: "uk_UA",
-        //width: 380,
-      },
-    )
+      // @ts-ignore
+      google.accounts.id.renderButton(
+        document.getElementById('google-login'),
+        {
+          theme: 'outline',
+          size: 'large',
+          locale: "uk_UA",
+          //width: 380,
+        },
+      )
+    }
   }, [])
 }
